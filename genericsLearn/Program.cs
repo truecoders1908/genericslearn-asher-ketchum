7b79613535be6f1b4dfbd59f4dd87514a49002ad
Adam Asher <asheradamr@gmail.com>
2019-09-23 10:21:43 -0500

    Made comments line from line to explain how the program works

--- genericsLearn/Program.cs
+++ genericsLearn/Program.cs
@@ -7,24 +7,26 @@ namespace genericsLearn
     {
         static void Main(string[] args)
         {
-            Bag<char> scrabbleLetters = new Bag<char>();
-            scrabbleLetters.PutItemIntoBag('q');
-            scrabbleLetters.PutItemIntoBag('j');
-            scrabbleLetters.PutItemIntoBag('z');
-            scrabbleLetters.PutItemIntoBag('y');
-            scrabbleLetters.PutItemIntoBag('x');
-            char letterFromBag = scrabbleLetters.GetItemFromBag();
-            Console.WriteLine(letterFromBag);
+            Bag<char> scrabbleLetters = new Bag<char>();                // The List Bag returns a char is being defined as scrabbleLetters 
+                                                                        // scrabbleLetters placed inside bag
+            scrabbleLetters.PutItemIntoBag('q');                        // Bag<char> == q
+            scrabbleLetters.PutItemIntoBag('j');                        // Bag<char> == j
+            scrabbleLetters.PutItemIntoBag('z');                        // Bag<char> == z
+            scrabbleLetters.PutItemIntoBag('y');                        // Bag<char> == y
+            scrabbleLetters.PutItemIntoBag('x');                        // Bag<char> == x
+            char letterFromBag = scrabbleLetters.GetItemFromBag();      // Defines Letter from bag as scrabbleLetters method GetItemFromBag [line 31]
+            Console.WriteLine(letterFromBag);                           // Console displaces a random scrabble piece from bag using the list (q,j,z,y,x)
         }
     }
 
-    class Bag<T>
+    class Bag<T>                                                        // New Class: Bag -- being defined as T
     {
-        private List<T> items = new List<T>();
+        private List<T> items = new List<T>();                          //[private] is only going to be shared inside this nest thats creating a new bag list instance
 
-        public void PutItemIntoBag(T item)
+        public void PutItemIntoBag(T item)                              // New Method: public void PutItemIntoBag(T item) 
+                                                                        // ***Bag = T*** 
         {
-            items.Add(item);
+            items.Add(item);                                            // 
         }
 
         public T GetItemFromBag()
